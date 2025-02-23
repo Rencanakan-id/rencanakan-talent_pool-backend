55db53a52c3f59316c68512f8270c3d9c89ad78f
Naomi <gabriella.naomi21@ui.ac.id>
2025-02-23 18:05:28 +0700

    ci for sonar cloud

--- src/test/java/rencanakan/id/talentPool/TalentPoolApplicationTests.java
+++ src/test/java/rencanakan/id/talentPool/TalentPoolApplicationTests.java
@@ -2,8 +2,10 @@ package rencanakan.id.talentPool;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TalentPoolApplicationTests {
 
 	@Test
