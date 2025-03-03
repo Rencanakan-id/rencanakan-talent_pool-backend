b08f16ec2415c1751d4374f19381a60b4caa45da
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2025-03-03 21:27:16 +0700

    [GREEN] implementation for getByTalentId in experience service

--- src/main/java/rencanakan/id/talentPool/service/ExperienceServiceImpl.java
+++ src/main/java/rencanakan/id/talentPool/service/ExperienceServiceImpl.java
@@ -4,14 +4,31 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rencanakan.id.talentPool.dto.EditExperienceRequestDTO;
+import rencanakan.id.talentPool.dto.ExperienceListResponseDTO;
 import rencanakan.id.talentPool.dto.ExperienceResponseDTO;
 import rencanakan.id.talentPool.model.Experience;
 import rencanakan.id.talentPool.repository.ExperienceRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ExperienceServiceImpl implements  ExperienceService{
     @Autowired
     ExperienceRepository experienceRepository;
+
+    @Override
+    public ExperienceListResponseDTO getByTalentId(Long id) {
+        List<Experience> experiences = experienceRepository.findByTalentId(id);
+        if (experiences.isEmpty()) {
+            throw new EntityNotFoundException("Experience is empty");
+        }else{
+            List<ExperienceResponseDTO> response = experiences.stream().map(this::toExperienceResponseDTO).collect(Collectors.toList());
+            return new ExperienceListResponseDTO(response);
+        }
+
+    }
+
     @Override
     public ExperienceResponseDTO editById(Long id, EditExperienceRequestDTO dto) {
         Experience experience = experienceRepository.findById(id)
