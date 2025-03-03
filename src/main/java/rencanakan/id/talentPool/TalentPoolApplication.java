ae9ccb8a4da9db3a353b48c1067ba2d978cb7d39
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2025-03-04 03:07:50 +0700

    [REFACTOR] delete some unused import for each file

--- src/main/java/rencanakan/id/talentPool/TalentPoolApplication.java
+++ src/main/java/rencanakan/id/talentPool/TalentPoolApplication.java
@@ -2,7 +2,6 @@ package rencanakan.id.talentPool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication()
 public class TalentPoolApplication {
