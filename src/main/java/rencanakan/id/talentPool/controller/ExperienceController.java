b98411201bfaac36b3b13033d0cf8e7406e66a4a
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2025-03-03 21:36:38 +0700

    [GREEN] implementation for experience controller in getExperienceByTalentsId

--- src/main/java/rencanakan/id/talentPool/controller/ExperienceController.java
+++ src/main/java/rencanakan/id/talentPool/controller/ExperienceController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import rencanakan.id.talentPool.dto.EditExperienceRequestDTO;
+import rencanakan.id.talentPool.dto.ExperienceListResponseDTO;
 import rencanakan.id.talentPool.dto.ExperienceResponseDTO;
 import rencanakan.id.talentPool.dto.WebResponse;
 import rencanakan.id.talentPool.service.ExperienceService;
@@ -13,6 +14,14 @@ import rencanakan.id.talentPool.service.ExperienceService;
 public class ExperienceController {
     @Autowired
     ExperienceService experienceService;
+
+    @GetMapping("/{talent_id}")
+    public WebResponse<ExperienceListResponseDTO> getExperiencesByTalentId(@PathVariable Long talent_id, @RequestHeader("Authorization") String token) {
+        ExperienceListResponseDTO resp = experienceService.getByTalentId(talent_id);
+        return WebResponse.<ExperienceListResponseDTO>builder()
+                .data(resp)
+                .build();
+    }
 
     @PutMapping("/{id}")
     public WebResponse<ExperienceResponseDTO> editExperienceById(@PathVariable Long id, @RequestHeader("Authorization") String token,   @Valid @RequestBody EditExperienceRequestDTO dto) {
