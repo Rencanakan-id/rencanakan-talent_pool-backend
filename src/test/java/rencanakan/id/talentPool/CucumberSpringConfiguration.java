af009dda77e3eeb9efb082759c450c32256adce2
William <williamsalimrjs@gmail.com>
2025-03-04 12:54:07 +0700

    [RED] initial behavior testing

---
+++ src/test/java/rencanakan/id/talentPool/CucumberSpringConfiguration.java
@@ -0,0 +1,10 @@
+package rencanakan.id.talentPool;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class CucumberSpringConfiguration {
+    // This class integrates Cucumber with Spring Boot
+}
