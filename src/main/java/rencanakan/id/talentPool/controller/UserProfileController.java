a7ab8b5fc69b4f3423b6d95d8f89fc5946f25851
Abbilville <abbilville@gmail.com>
2025-03-09 17:22:46 +0700

    [GREEN] implement user profile controller

--- src/main/java/rencanakan/id/talentPool/controller/UserProfileController.java
+++ src/main/java/rencanakan/id/talentPool/controller/UserProfileController.java
@@ -1,4 +1,24 @@
 package rencanakan.id.talentPool.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import rencanakan.id.talentPool.dto.UserProfileResponseDTO;
+import rencanakan.id.talentPool.dto.WebResponse;
+import rencanakan.id.talentPool.service.UserProfileService;
+
+@RestController
+@RequestMapping("/api/user-profiles")
 public class UserProfileController {
+
+    @Autowired
+    private UserProfileService userProfileService;
+
+    @GetMapping("/{id}")
+    public WebResponse<UserProfileResponseDTO> getUserProfileById(@PathVariable String id, @RequestHeader("Authorization") String token) {
+        UserProfileResponseDTO resp = userProfileService.getById(id);
+        return WebResponse.<UserProfileResponseDTO>builder()
+                .data(resp)
+                .build();
+    }
 }
