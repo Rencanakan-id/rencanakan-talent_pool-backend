3b805849499cccb33fde34b389f8ff5c6262f845
Abbilville <abbilville@gmail.com>
2025-03-09 17:21:58 +0700

    [GREEN] implement user profile response dto

---
+++ src/main/java/rencanakan/id/talentPool/dto/UserProfileResponseDTO.java
@@ -0,0 +1,34 @@
+package rencanakan.id.talentPool.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserProfileResponseDTO {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String job;
+    private String photo;
+    private String aboutMe;
+    private String nik;
+    private String npwp;
+    private String photoKtp;
+    private String photoNpwp;
+    private String photoIjazah;
+    private Integer experienceYears;
+    private String skkLevel;
+    private String currentLocation;
+    private List<String> preferredLocations;
+    private String skill;
+}
