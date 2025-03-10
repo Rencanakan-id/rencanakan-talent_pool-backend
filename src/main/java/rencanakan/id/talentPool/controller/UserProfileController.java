7d66771b8ef4a9f6a9a55e494ba2a1d36c3692a5
Scarletra <fernandovalentino10@gmail.com>
2025-03-10 11:11:29 +0700

    [GREEN] Implement new path for edit profile feat on controller

--- src/main/java/rencanakan/id/talentPool/controller/UserProfileController.java
+++ src/main/java/rencanakan/id/talentPool/controller/UserProfileController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import rencanakan.id.talentPool.dto.UserProfileResponseDTO;
 import rencanakan.id.talentPool.dto.WebResponse;
+import rencanakan.id.talentPool.model.UserProfile;
 import rencanakan.id.talentPool.service.UserProfileService;
 
 @RestController
@@ -19,6 +20,19 @@ public class UserProfileController {
         UserProfileResponseDTO resp = userProfileService.getById(id);
         return WebResponse.<UserProfileResponseDTO>builder()
                 .data(resp)
+                .build();
+    }
+
+    @PutMapping("/edit/{id}")
+    public WebResponse<UserProfileResponseDTO> editUserProfile(
+            @PathVariable String id,
+            @RequestBody UserProfile editedProfile,
+            @RequestHeader("Authorization") String token) {
+
+        UserProfileResponseDTO updatedProfile = userProfileService.editProfile(id, editedProfile);
+
+        return WebResponse.<UserProfileResponseDTO>builder()
+                .data(updatedProfile)
                 .build();
     }
 }
