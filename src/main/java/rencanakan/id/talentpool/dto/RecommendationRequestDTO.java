527f6817dfc9a3873841b00a07c0a8b290fb8b8b
raviehasan <ravie.hasan@ui.ac.id>
2025-03-20 17:41:56 +0700

    [GREEN] implements request-response DTO

--- src/main/java/rencanakan/id/talentpool/dto/RecommendationRequestDTO.java
+++ src/main/java/rencanakan/id/talentpool/dto/RecommendationRequestDTO.java
@@ -1,5 +1,8 @@
 package rencanakan.id.talentpool.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import rencanakan.id.talentpool.enums.StatusType;
 
@@ -8,9 +11,17 @@ import rencanakan.id.talentpool.enums.StatusType;
 @Builder
 @AllArgsConstructor
 public class RecommendationRequestDTO {
+    @NotNull(message = "Contractor ID is required")
     private Long contractorId;
+    
+    @NotBlank(message = "Contractor name is required")
     private String contractorName;
+
+    @NotBlank(message = "Message is required")
+    @Size(max = 4000, message = "Message cannot exceed 4000 characters")
     private String message;
+
+    @NotNull(message = "Status is required")
     private StatusType status;
 
     public RecommendationRequestDTO() { }
