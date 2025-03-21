1472c4888c4d7b29ac8a9ad39728fbfd5b434efe
raviehasan <ravie.hasan@ui.ac.id>
2025-03-21 13:08:24 +0700

    [GREEN] changed User to ID on DTO

--- src/main/java/rencanakan/id/talentpool/dto/RecommendationResponseDTO.java
+++ src/main/java/rencanakan/id/talentpool/dto/RecommendationResponseDTO.java
@@ -2,7 +2,6 @@ package rencanakan.id.talentpool.dto;
 
 import lombok.*;
 import rencanakan.id.talentpool.enums.StatusType;
-import rencanakan.id.talentpool.model.User;
 
 @Getter
 @Setter
@@ -11,7 +10,7 @@ import rencanakan.id.talentpool.model.User;
 @NoArgsConstructor
 public class RecommendationResponseDTO {
     private String id;
-    private User talent;
+    private String talentId;
     private Long contractorId;
     private String contractorName;
     private String message;
