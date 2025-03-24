70f8e16af03d63680deed08cd065ed164f5705df
William <williamsalimrjs@gmail.com>
2025-03-24 23:38:04 +0700

    [GREEN] implement edit recom controller

---
+++ src/main/java/rencanakan/id/talentpool/controller/RecommendationController.java
@@ -0,0 +1,34 @@
+package rencanakan.id.talentpool.controller;
+
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rencanakan.id.talentpool.dto.*;
+import rencanakan.id.talentpool.repository.RecommendationRepository;
+import rencanakan.id.talentpool.service.RecommendationServiceImpl;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recommendations")
+public class RecommendationController {
+    private RecommendationServiceImpl recommendationService;
+
+    public RecommendationController(RecommendationServiceImpl recommendationService) {
+        this.recommendationService = recommendationService;
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<WebResponse<RecommendationResponseDTO>> editRecommendationById(
+            @PathVariable Long id,
+            @RequestBody @Valid RecommendationRequestDTO dto) {
+
+        RecommendationResponseDTO updatedRecommendation = recommendationService.editById(id, dto);
+
+        return ResponseEntity.ok(WebResponse.<RecommendationResponseDTO>builder()
+                .data(updatedRecommendation)
+                .build());
+    }
+}
