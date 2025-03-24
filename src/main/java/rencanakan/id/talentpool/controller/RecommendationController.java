ddb4af5014f6951999b090defc9e87cea35d127d
Naomi <gabriella.naomi21@ui.ac.id>
2025-03-24 11:15:18 +0700

    [GREEN] implement edit status controller

---
+++ src/main/java/rencanakan/id/talentpool/controller/RecommendationController.java
@@ -0,0 +1,36 @@
+package rencanakan.id.talentpool.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import rencanakan.id.talentpool.dto.*;
+import rencanakan.id.talentpool.model.User;
+import rencanakan.id.talentpool.service.ExperienceServiceImpl;
+import rencanakan.id.talentpool.service.RecommendationService;
+
+@RestController
+@RequestMapping("/recommendations")
+public class RecommendationController {
+    private final RecommendationService recommendationService;
+
+    public RecommendationController(RecommendationService recommendationService) {
+        this.recommendationService = recommendationService;
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<WebResponse<RecommendationResponseDTO>> editStatusById(
+            @PathVariable String id,
+            @RequestBody @Valid RecommendationStatusRequestDTO dto){
+        RecommendationResponseDTO res = this.recommendationService.editStatusById(id, dto);
+        WebResponse<RecommendationResponseDTO> response = WebResponse.<RecommendationResponseDTO>builder()
+                .data(res)
+                .build();
+
+        return ResponseEntity.ok(response);
+
+    }
+
+}
