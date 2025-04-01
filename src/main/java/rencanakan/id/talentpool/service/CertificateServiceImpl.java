e239e3774e6d40ebf0af32debdd598e31fcf5b53
Abbilville <abbilville@gmail.com>
2025-04-01 13:33:23 +0700

    [GREEN] implement read certificate services

---
+++ src/main/java/rencanakan/id/talentpool/service/CertificateServiceImpl.java
@@ -0,0 +1,44 @@
+package rencanakan.id.talentpool.service;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityNotFoundException;
+import rencanakan.id.talentpool.dto.CertificateResponseDTO;
+import rencanakan.id.talentpool.mapper.DTOMapper;
+import rencanakan.id.talentpool.model.Certificate;
+import rencanakan.id.talentpool.repository.CertificateRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CertificateServiceImpl implements CertificateService {
+
+    private final CertificateRepository certificateRepository;
+
+    public CertificateServiceImpl(CertificateRepository certificateRepository) {
+        this.certificateRepository = certificateRepository;
+    }
+
+    @Override
+    public List<CertificateResponseDTO> getByUserId(String talentId) {
+
+        List<Certificate> certificates = certificateRepository.findByUserId(talentId);
+        if (certificates.isEmpty()) {
+            throw new EntityNotFoundException("Certificates not found for user with id " + talentId);
+        }
+
+        return certificates.stream()
+                .map(certificate -> DTOMapper.map(certificate, CertificateResponseDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CertificateResponseDTO getById(Long certificateId) {
+
+        Certificate certificate = certificateRepository.findById(certificateId)
+                .orElseThrow(() -> new EntityNotFoundException("Certificate with id " + certificateId + " not found"));
+
+        return DTOMapper.map(certificate, CertificateResponseDTO.class);
+    }
+}
