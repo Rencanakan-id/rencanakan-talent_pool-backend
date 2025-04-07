089d21e382cab783540490084575cd6496914860
William <williamsalimrjs@gmail.com>
2025-04-07 20:40:27 +0700

    [REFACTOR] remove unused import

--- src/test/java/rencanakan/id/talentpool/unit/service/AuthenticationServiceTest.java
+++ src/test/java/rencanakan/id/talentpool/unit/service/AuthenticationServiceTest.java
@@ -6,7 +6,6 @@ import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.apache.coyote.BadRequestException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
