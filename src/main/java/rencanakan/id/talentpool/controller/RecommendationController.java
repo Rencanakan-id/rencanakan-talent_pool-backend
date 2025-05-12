bc32e935db3991a47f9c0e9d32fc377d9be55c6a
meteoreinger <reikathr@gmail.com>
2025-05-12 23:52:03 +0700

    chore: remove unused import in recom controller

--- src/main/java/rencanakan/id/talentpool/controller/RecommendationController.java
+++ src/main/java/rencanakan/id/talentpool/controller/RecommendationController.java
@@ -1,20 +1,15 @@
 package rencanakan.id.talentpool.controller;
 
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import rencanakan.id.talentpool.dto.RecommendationRequestDTO;import org.springframework.web.server.ResponseStatusException;
+import rencanakan.id.talentpool.dto.RecommendationRequestDTO;
 import rencanakan.id.talentpool.model.User;
 import rencanakan.id.talentpool.dto.*;
 import rencanakan.id.talentpool.enums.StatusType;
 import rencanakan.id.talentpool.service.RecommendationService;
-import rencanakan.id.talentpool.service.UserService;
 
 import java.util.List;
 import java.util.Map;
