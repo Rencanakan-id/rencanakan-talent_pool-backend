485c421d2ba753187fde901431a56d543e11fa1d
William <williamsalimrjs@gmail.com>
2025-05-12 23:34:36 +0700

    [REFACTOR] remove unused import

--- src/main/java/rencanakan/id/talentpool/controller/EmailController.java
+++ src/main/java/rencanakan/id/talentpool/controller/EmailController.java
@@ -1,6 +1,5 @@
 package rencanakan.id.talentpool.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rencanakan.id.talentpool.service.EmailService;
