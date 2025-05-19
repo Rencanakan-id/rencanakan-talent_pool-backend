51cd0ba6f0791f9ad95a67295cff1146de23bcbb
William <williamsalimrjs@gmail.com>
2025-05-19 23:18:23 +0700

    [REFACTOR] implement email 404 validation

--- src/main/java/rencanakan/id/talentpool/controller/EmailController.java
+++ src/main/java/rencanakan/id/talentpool/controller/EmailController.java
@@ -1,19 +1,17 @@
 package rencanakan.id.talentpool.controller;
 
 import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rencanakan.id.talentpool.service.EmailService;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/email")
 public class EmailController {
 
     private final EmailService emailService;
-
-    public EmailController(EmailService emailService) {
-        this.emailService = emailService;
-    }
 
     @PostMapping
     public ResponseEntity<String> sendResetPassword(@RequestBody ResetPasswordRequest request) {
