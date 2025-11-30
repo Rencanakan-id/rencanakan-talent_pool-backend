15c59ea0153975e3c70921eee585d58783279845
Filbert Hartawan <filberthtwn@gmail.com>
2025-11-30 10:22:58 +0700

    fix: missing import

--- src/main/java/rencanakan/id/talentpool/configs/SecurityConfiguration.java
+++ src/main/java/rencanakan/id/talentpool/configs/SecurityConfiguration.java
@@ -14,7 +14,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Arrays;
 import java.util.List;
 
 
